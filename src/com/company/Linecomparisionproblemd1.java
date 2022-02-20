865a9fbd45601f00c700063fe41df6c8f96093a6
Pooja Sree <you@example.com>
2022-02-20 21:10:42 +0530

    added welcome message

---
+++ src/com/company/Linecomparisionproblemd1.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Linecomparisionproblemd1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparision problem");
+    }
+}
